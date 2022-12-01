5dd7535a3c9713cc0dc81042a8e2c0f35b98003a
Axelle <guinaudeau.ion1516@gmail.com>
2022-12-01 14:10:16 +0100

    :sparkles: add dashboard page

---
+++ front/src/pages/DashboardPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="q-pa-xl">
+    <h2>Dashboard</h2>
+    <!-- <List></List> -->
+  </div>
+</template>
+<script>
+// import List from 'components/list/listComponent.vue'
+
+// Syntax vue2 Option API
+export default {
+  data: () => ({
+  }),
+  mounted () {
+    console.log('Component mounted')
+  },
+  async created () {
+    console.log('Component created')
+  },
+  computed: {
+  },
+  methods: {
+  },
+  watch: {
+  },
+  components: {
+    // List
+  }
+}
+</script>
