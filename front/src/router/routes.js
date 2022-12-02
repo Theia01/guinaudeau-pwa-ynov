5e96c380e25f74494197731404c369bd4d42dbab
Axelle <guinaudeau.ion1516@gmail.com>
2022-12-02 11:13:20 +0100

    :truck: change route dashboard

--- front/src/router/routes.js
+++ front/src/router/routes.js
@@ -3,8 +3,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'dashboard', component: () => import('pages/DashboardPage.vue') }
+      { path: '', component: () => import('pages/DashboardPage.vue') },
+      { path: 'index', component: () => import('pages/IndexPage.vue') }
     ]
   },
   {
