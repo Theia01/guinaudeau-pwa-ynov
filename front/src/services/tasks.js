b6eb16e12ee368b0d80b2dcd5d85a117c38e9c61
Axelle <guinaudeau.ion1516@gmail.com>
2022-12-04 14:25:22 +0100

    :alien: add new route from api

--- front/src/services/tasks.js
+++ front/src/services/tasks.js
@@ -3,3 +3,7 @@ import { api } from 'boot/axios'
 export function getAllTasks () {
   return api.get('/tasks')
 }
+
+export function getAllTasksfromList (id) {
+  return api.get('/tasks/list/' + id)
+}
