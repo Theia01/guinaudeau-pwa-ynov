dcdfc5d823bc796d9963a2a1b293dbf347741bcc
Axelle <guinaudeau.ion1516@gmail.com>
2022-12-09 09:07:49 +0100

    :sparkles: add links in menu

--- front/src/layouts/MainLayout.vue
+++ front/src/layouts/MainLayout.vue
@@ -30,7 +30,7 @@
         </q-item-label>
 
         <EssentialLink
-          v-for="link in essentialLinks"
+          v-for="link in linksList"
           :key="link.title"
           v-bind="link"
         />
@@ -81,54 +81,11 @@
 <script>
 import { defineComponent, ref } from 'vue'
 import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+import * as ServiceLists from './../services/lists'
 
 export default defineComponent({
   name: 'MainLayout',
+  linksList: [],
 
   components: {
     EssentialLink
@@ -138,12 +95,24 @@ export default defineComponent({
     const leftDrawerOpen = ref(false)
 
     return {
-      essentialLinks: linksList,
       leftDrawerOpen,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
       }
     }
+  },
+  async created () {
+    const { data } = await ServiceLists.getAllLists()
+
+    const linksList = []
+    data.forEach(item => {
+      linksList.push({
+        title: `${item.title}`,
+        link: window.location = '#/list/' + item._id + '/'
+      })
+    })
+
+    this.linksList = linksList
   }
 })
 </script>
